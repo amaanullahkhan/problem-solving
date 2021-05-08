ba6a0d9f75fd2b18f4ff0e6850788eaac2840f64
amaanullahkhan <amaankhanse@gmail.com>
2021-05-09 00:03:46 +0500

    - merge sort: counting inversion

--- HackerRank/InterviewPreparationKit.playground/Contents.swift
+++ HackerRank/InterviewPreparationKit.playground/Contents.swift
@@ -462,57 +462,65 @@ func activityNotifications(expenditure: [Int], d: Int) -> Int {
 
 // Complete the countInversions function below.
 
-func countInversions(arr: [Int]) -> Int {
+func countInversions( arr: inout [Int]) -> Int {
     
     var inversion = 0
     
-    func mergeSort(array: [Int]) -> [Int] {
-        guard  array.count > 1 else {
-            return array
+    func mergeSort(array: inout [Int], temp: inout [Int], start: Int, end: Int) {
+        
+        guard  start < end else {
+            return
         }
-        let mid = array.count / 2
-        let left = mergeSort(array: Array(array[0..<mid]))
-        let right = mergeSort(array: Array(array[mid...]))
-        return merge(left: left, right: right)
+        
+        let mid = (start + end) / 2
+        mergeSort(array: &array, temp: &temp, start: start, end: mid)
+        mergeSort(array: &array, temp: &temp, start: mid+1, end: end)
+        merge(array: &array, temp: &temp, start: start, mid: mid, end: end)
     }
 
-    func merge(left: [Int], right: [Int]) -> [Int] {
-    
-        var result: [Int] = []
-        result.reserveCapacity(left.count + right.count)
+    func merge(array: inout [Int], temp: inout [Int], start: Int, mid: Int, end: Int) {
 
-        var leftIndex = 0
-        var rightIndex = 0
+        var leftIndex = start
+        var rightIndex = mid + 1
+        var index = start
 
-        while leftIndex < left.count && rightIndex < right.count {
-            if left[leftIndex] <= right[rightIndex] {
-                result.append(left[leftIndex])
+        while leftIndex <= mid && rightIndex <= end {
+            if array[leftIndex] <= array[rightIndex] {
+                temp[index] = array[leftIndex]
                 leftIndex += 1
             } else {
-                result.append(right[rightIndex])
-                inversion += left.count - leftIndex
+                temp[index] = array[rightIndex]
+                inversion += (mid + 1) - leftIndex
                 rightIndex += 1
             }
+            index += 1
         }
 
-        while leftIndex < left.count {
-            result.append(left[leftIndex])
+        while leftIndex <= mid {
+            temp[index] = array[leftIndex]
             leftIndex += 1
+            index += 1
         }
 
-        while rightIndex < right.count {
-            result.append(right[rightIndex])
+        while rightIndex <= end {
+            temp[index] = array[rightIndex]
             rightIndex += 1
+            index += 1
         }
-
-        return result
+        
+        array[start...end] = temp[start...end]
     }
     
-    mergeSort(array: arr)
+    var temp = arr
     
+    mergeSort(array: &arr, temp: &temp, start: 0, end: arr.count-1)
+        
     return inversion
 }
 
-countInversions(arr: [2, 1, 3, 1, 2])
-countInversions(arr: [2,4,1])
-countInversions(arr: [1, 1, 1, 2, 2])
+
+var array = [2, 1, 3, 1, 2]
+countInversions(arr: &array)
+array
+//countInversions(arr: [2,4,1])
+//countInversions(arr: [1, 1, 1, 2, 2])
