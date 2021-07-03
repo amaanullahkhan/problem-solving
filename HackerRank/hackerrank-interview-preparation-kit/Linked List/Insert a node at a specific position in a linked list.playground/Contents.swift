ca2881b7b4c9e1759bab67dcde363ed60c9c4ad1
amaanullahkhan <amaankhanse@gmail.com>
2021-07-03 19:42:41 +0500

    Insert a node at a specific position in a linked list

---
+++ HackerRank/hackerrank-interview-preparation-kit/Linked List/Insert a node at a specific position in a linked list.playground/Contents.swift
@@ -0,0 +1,87 @@
+import Foundation
+
+final class SinglyLinkedListNode {
+    var data: Int
+    var next: SinglyLinkedListNode?
+
+    public init(nodeData: Int) {
+        self.data = nodeData
+    }
+}
+
+final class SinglyLinkedList {
+    var head: SinglyLinkedListNode?
+    var tail: SinglyLinkedListNode?
+
+    public init() {}
+
+    public func insertNode(nodeData: Int) {
+        self.insertNode(node: SinglyLinkedListNode(nodeData: nodeData))
+    }
+
+    private func insertNode(node: SinglyLinkedListNode) {
+        if let tail = tail {
+            tail.next = node
+        } else {
+            head = node
+        }
+
+        tail = node
+    }
+}
+
+func printSinglyLinkedList(head: SinglyLinkedListNode?, sep: String, fileHandle: FileHandle) {
+    var node = head
+
+    while node != nil {
+        fileHandle.write(String(node!.data).data(using: .utf8)!)
+
+        node = node!.next
+
+        if node != nil {
+            fileHandle.write(sep.data(using: .utf8)!)
+        }
+    }
+}
+
+/*
+ * Complete the 'insertNodeAtPosition' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER data
+ *  3. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data: Int
+ *     next: SinglyLinkedListNode?
+ * }
+ *
+ */
+
+func insertNodeAtPosition(llist: SinglyLinkedListNode?, data: Int, position: Int) -> SinglyLinkedListNode? {
+    let new = SinglyLinkedListNode(nodeData: data)
+    var previous: SinglyLinkedListNode?
+    var i = 0
+    while i <= position {
+        if i == position {
+            new.next = previous?.next
+            previous?.next = new
+            break
+        }
+        i+=1;
+        if previous == nil {
+            previous = llist
+        } else {
+            previous = previous?.next
+        }
+    }
+    return llist
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
