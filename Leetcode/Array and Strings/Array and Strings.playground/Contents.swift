916a6f095124a75b1254c7e255f6d774e04abf23
Amaan Ullah <amaankhanse@gmail.com>
2024-11-04 19:20:17 +0500

    Array: rotate

--- Leetcode/Array and Strings/Array and Strings.playground/Contents.swift
+++ Leetcode/Array and Strings/Array and Strings.playground/Contents.swift
@@ -1,5 +1,76 @@
 import UIKit
 
+
+/*
+ Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+  
+
+ Example 1:
+
+ Input: nums = [1,2,3,4,5,6,7], k = 3
+ Output: [5,6,7,1,2,3,4]
+ Explanation:
+ rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ Example 2:
+
+ Input: nums = [-1,-100,3,99], k = 2
+ Output: [3,99,-1,-100]
+ Explanation:
+ rotate 1 steps to the right: [99,-1,-100,3]
+ rotate 2 steps to the right: [3,99,-1,-100]
+  
+
+ Constraints:
+
+ 1 <= nums.length <= 105
+ -231 <= nums[i] <= 231 - 1
+ 0 <= k <= 105
+  
+
+ Follow up:
+
+ Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+ Could you do it in-place with O(1) extra space?
+ */
+
+func rotate(_ nums: inout [Int], _ k: Int) {
+    let rotation = k % nums.count
+    guard rotation > 0 else {
+        return
+    }
+    nums = nums.suffix(rotation) + nums.prefix(nums.count-rotation)
+}
+
+func rotateInPlace(_ nums: inout [Int], _ k: Int) {
+        let rotation = k % nums.count
+        reverse(&nums, 0, nums.count-1)
+        reverse(&nums, 0, rotation-1)
+        reverse(&nums, rotation, nums.count-1)
+    }
+    
+    func reverse(_ nums: inout [Int], _ start: Int, _ end: Int) {
+        var from = start
+        var to = end
+        while from < to {
+            let num = nums[to]
+            nums[to] = nums[from]
+            nums[from] = num
+            from += 1
+            to -= 1
+        }
+    }
+var roatationArray = [-1,-100,3,99]
+var rotationArray2 = [1,2,3,4,5,6,7,8,9,10]
+var rotationArray3 = [1,2]
+
+rotateInPlace(&roatationArray, 2)
+rotateInPlace(&rotationArray2, 3)
+rotateInPlace(&rotationArray3, 3)
+
+
 /*
  You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 
